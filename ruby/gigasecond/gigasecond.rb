c6e12f4e1a4aabcf2d75af3d67bff75c892a470b
Hashrocket Workstation <dev@hashrocket.com>
2015-01-26 15:02:42 -0500

    gigasecond completed as well as rna-transcription

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,10 +1,13 @@
+##Gigasecond
+##John Youngblood
+##1/26/2015
 require 'date'
 require 'time'
 
 class Gigasecond
   def self.from(day)
     @gigas = 10**9
-    day.to_time if gigaday.is_a? Date
+    day = day.to_time if day.is_a? Date
     temp = day+@gigas
     gigaday = Date.new(temp.year,temp.mon,temp.day)
   end
--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,14 +1,10 @@
+##Hamming Distance (http://exercism.io/exercises/ruby/hamming/readme)
+##John Youngblood
+##1/26/2015
 class Hamming
   def self.compute(arg1, arg2)
-    unless arg1.length == arg2.length
-      puts "DNA strands are not of equal length."
-      0
-    else
-      hammDis = 0
-      for i in 0...arg1.length
-	hammDis+=1 unless arg1[i] == arg2[i]
-      end
-      hammDis
-    end
+    hammDis = 0
+    arg1.length.times{|i| hammDis+=1 unless arg1[i] == arg2[i]}
+    hammDis
   end
 end
