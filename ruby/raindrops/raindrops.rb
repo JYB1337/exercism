fdd31b04482f4710974addd2a3aee7603fffb517
Hashrocket Workstation <dev@hashrocket.com>
2015-01-27 11:54:46 -0500

    h

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -3,12 +3,11 @@
 ##1/26/2015
 class Raindrops
   def self.convert(num)
-    rain = ""
-    rain << "Pling" if num % 3 == 0; rain << "Plang" if num % 5 == 0; rain << "Plong" if num % 7 == 0
-    if rain == ""
-      num.to_s
-    else
-      rain
+    "".tap do |rain|
+      rain << "Pling" if num % 3 == 0
+      rain << "Plang" if num % 5 == 0
+      rain << "Plong" if num % 7 == 0
+      rain << num.to_s if rain.empty?
     end
   end
 end
--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -3,9 +3,9 @@
 ##1/26/2015
 class Complement
   def self.of_dna(dna)
-    dna.gsub(/[g]/imx, 'X').gsub(/[c]/imx, 'G').gsub(/[a]/imx, 'U').gsub(/[t]/imx, 'A').gsub(/[x]/imx, 'C')
+    dna.gsub(/[GCTA]/, { 'G' => 'C', 'C' => 'G', 'A' => 'U', 'T' => 'A'})
   end
   def self.of_rna(rna)
-    rna.gsub(/[g]/imx, 'X').gsub(/[c]/imx, 'G').gsub(/[a]/imx, 'T').gsub(/[u]/imx, 'A').gsub(/[x]/imx, 'C')
+    rna.gsub(/[GCAU]/, { 'G' => 'C', 'C' => 'G', 'U' => 'A', 'A' => 'T'})
   end
 end
