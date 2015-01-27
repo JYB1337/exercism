bd76d1c10a125ae25fcc8d511b6efcab6c056657
Hashrocket Workstation <dev@hashrocket.com>
2015-01-27 14:27:52 -0500

    1/27/2015

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -4,7 +4,10 @@
 class Hamming
   def self.compute(arg1, arg2)
     hammDis = 0
-    arg1.length.times{|i| hammDis+=1 unless arg1[i] == arg2[i]}
+    ##arg1.length.times{|i| hammDis+=1 unless arg1[i] == arg2[i]}
+    arg1.length.times do |i|
+      hammDis+=1 unless arg1[i] == arg2[i]
+    end
     hammDis
   end
 end
