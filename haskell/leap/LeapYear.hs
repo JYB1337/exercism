77c8bdd48da3c1e6f176207bc039db4ef1109c64
Hashrocket Workstation <dev@hashrocket.com>
2015-02-06 13:06:50 -0500

    first Haskell exercise done

--- haskell/leap/LeapYear.hs
+++ haskell/leap/LeapYear.hs
@@ -1,4 +1,9 @@
 --John Youngblood
 --2/02/2015
+module LeapYear where
 isLeapYear :: Int -> Bool
-
+isLeapYear y
+  | mod y 400 == 0 = True
+    | mod y 100 == 0 = False
+      | mod y 4 == 0 = True
+        | otherwise = False
---
+++ haskell/leap/dist/build/autogen/Paths_leap.hs
@@ -0,0 +1,34 @@
+module Paths_leap (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+
+version :: Version
+version = Version {versionBranch = [0,1,0,0], versionTags = []}
+bindir, libdir, datadir, libexecdir :: FilePath
+
+bindir     = "/home/youngblood/.cabal/bin"
+libdir     = "/home/youngblood/.cabal/lib/leap-0.1.0.0/ghc-7.6.3"
+datadir    = "/home/youngblood/.cabal/share/leap-0.1.0.0"
+libexecdir = "/home/youngblood/.cabal/libexec"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir :: IO FilePath
+getBinDir = catchIO (getEnv "leap_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "leap_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "leap_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "leap_libexecdir") (\_ -> return libexecdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
