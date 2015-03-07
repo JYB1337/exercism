a441166a2735846c2b75575232f2b8a8cae70384
John Youngblood <jyb1337@gmail.com>
2015-03-07 16:48:19 -0500

    setting up Javascript/Coffescript test suites.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,19 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+//John Youngblood
+//3-09-2015
 
-var Bob = function() {};
+var Bob = function () { };
 
-Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+Bob.prototype.hey = function (input) {
+
+    var output = "Whatever.";
+
+    if (input === input.toUpperCase() && input !== input.toLowerCase())
+        output = "Whoa, chill out!";
+    else if (input.charAt(input.length - 1) === '?')
+        output = "Sure."
+    else if (input.trim() === "")
+        output = "Fine. Be that way!"
+    return output;
 };
 
 module.exports = Bob;
---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,20 @@
+﻿//John Youngblood
+//3-07-2015
+var words = function (input)
+{
+    var output = {};
+    var wordList = input.replace(/\s+/g, " ").split(" ");
+    while (wordList.length > 0)
+    {
+        var temp = wordList.shift();
+        output[temp] = 1;
+        while(wordList.indexOf(temp) != -1)
+        {
+            wordList.splice(wordList.indexOf(temp),1)
+            output[temp]++;
+        }
+    }
+    return output;
+}
+
+module.exports = words;
